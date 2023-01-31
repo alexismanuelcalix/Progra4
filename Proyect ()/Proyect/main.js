30ab6533c1f3d64351fcb235a112d9dfbf3809de
Alexis Manuel <alexismanuel7270@gmail.com>
2023-01-30 23:44:32 -0600

    Actualizar

---
+++ Proyect ()/Proyect/main.js
@@ -0,0 +1,11 @@
+//agregar elementos a la lista
+const addItemInput = document.querySelector('#addItem')
+const addItemButton = document.querySelector('button#addItemButton')
+
+addItemButton.addEventListener('click', () =>{
+    let list = document.querySelector('')
+    let li = document.createElement('')
+    li.textContent = addItemInput.value
+    list.appendChild(l)
+
+}) 
